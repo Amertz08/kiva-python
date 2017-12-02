149d41f5c9edbea569eb73df13fff0cdbc72fdb4
Adam Mertz <adammertz@gmail.com>
2017-12-02 00:58:59 -0600

    Wrote test for loans

--- test_api.py
+++ test_api.py
@@ -4,3 +4,15 @@ import kiva
 def test_recent_lending_actions():
     results = kiva.recent_lending_actions()
     assert isinstance(results, kiva.KivaList), f'{type(results)} is not {type(kiva.KivaList)}'
+
+
+def test_loans():
+    loans = kiva.recent_lending_actions()
+    if not loans:
+        assert False, 'No loans found'
+    else:
+        n = 10 if len(loans) > 10 else len(loans)
+        loan_ids = [entry.loan.id for entry in loans]
+        results = kiva.loans(loan_ids[:n])
+        assert isinstance(results, kiva.KivaList), f'{type(results)} is not {type(kiva.KivaList)}'
+
