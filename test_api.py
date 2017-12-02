81fe6ebe4a2ce02fad9d6e48396ce18d103aad70
Adam Mertz <adammertz@gmail.com>
2017-12-02 01:36:30 -0600

    Wrote test case for lender_loans

--- test_api.py
+++ test_api.py
@@ -1,3 +1,4 @@
+import pprint
 import kiva
 
 
@@ -12,7 +13,16 @@ def test_loans():
         assert False, 'No loans found'
     else:
         n = 10 if len(loans) > 10 else len(loans)
-        loan_ids = [entry.loan.id for entry in loans]
-        results = kiva.loans(loan_ids[:n])
+        pprint.pprint(loans[:8])
+        loan_ids = [entry.loan.id for entry in loans[:n]]
+        results = kiva.loans(loan_ids)
         assert isinstance(results, kiva.KivaList), f'{type(results)} is not {type(kiva.KivaList)}'
 
+
+def test_lender_loans():
+    loans = kiva.lender_loans('metlifefoundation3229')
+    assert isinstance(loans, kiva.KivaList), 'Loans should return KivaList'
+    assert isinstance(loans[0], kiva.KivaContainer), 'Loan items should be KivaContainers'
+    assert loans.page == 1, f'Should be on page 1: {loans.page}'
+    next_loans = loans.get_next_page()
+    assert next_loans.page == 2, f'Should be on page 2: {next_loans.page}'
