cbca90c60641fafa67cf21958a4f252c96b9d5d1
Adam Mertz <adammertz@gmail.com>
2017-12-02 19:18:12 -0600

    Moved KivaAPI object into its own module to reduce amount of names in actual package

--- kiva/__init__.py
+++ kiva/__init__.py
@@ -1,44 +1,7 @@
-from .base import BaseAPI, KivaContainer, KivaList
-from .lenders import Lenders
-from .lending_actions import LendingActions
-from .loans import Loans
+from .base import KivaContainer, KivaList
+from .kiva import KivaAPI
 
 __version__ = 0.1
-
-
-class KivaAPI(BaseAPI):
-
-    @property
-    def lenders(self):
-        return Lenders(self.app_id, self.version)
-
-    @property
-    def lending_actions(self):
-        return LendingActions(self.app_id, self.version)
-
-    @property
-    def loans(self):
-        return Loans(self.app_id, self.version)
-
-    @property
-    def methods(self):
-        raise NotImplementedError('KivaAPI.methods not implemented')
-
-    @property
-    def my(self):
-        raise NotImplementedError('KivaAPI.my not implemented')
-
-    @property
-    def partners(self):
-        raise NotImplementedError('KivaAPI.partners not implemented')
-
-    @property
-    def teams(self):
-        raise NotImplementedError('KivaAPI.teams not implemented')
-
-    @property
-    def templates(self):
-        raise NotImplementedError('KivaAPI.templates not implemented')
 
 # TODO: cannot find on API docs
 # def entry_comments(entry_id, page=1):
---
+++ kiva/kiva.py
@@ -0,0 +1,38 @@
+
+from .lenders import Lenders
+from .lending_actions import LendingActions
+from .loans import Loans
+
+class KivaAPI(BaseAPI):
+
+    @property
+    def lenders(self):
+        return Lenders(self.app_id, self.version)
+
+    @property
+    def lending_actions(self):
+        return LendingActions(self.app_id, self.version)
+
+    @property
+    def loans(self):
+        return Loans(self.app_id, self.version)
+
+    @property
+    def methods(self):
+        raise NotImplementedError('KivaAPI.methods not implemented')
+
+    @property
+    def my(self):
+        raise NotImplementedError('KivaAPI.my not implemented')
+
+    @property
+    def partners(self):
+        raise NotImplementedError('KivaAPI.partners not implemented')
+
+    @property
+    def teams(self):
+        raise NotImplementedError('KivaAPI.teams not implemented')
+
+    @property
+    def templates(self):
+        raise NotImplementedError('KivaAPI.templates not implemented')
